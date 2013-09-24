0b6c6dcb56f0dff5846fdd74d17b1b05a4bad3d5
Alexandre Thome <ale.thome@gmail.com>
2013-09-24 16:43:33 -0400

    added myway assets and html

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Bp::Application.routes.draw do
   root :to => "home#index"
+  get 'blog' => "blog#index"
   devise_for :users, :controllers => {:registrations => "registrations"}
   resources :users
 end
